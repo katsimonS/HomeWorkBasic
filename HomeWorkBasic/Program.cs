8e0acc97a1a484b4d71484b228e65ab0d0e515bd
Serhii Katsimon <jora.proforg@gmail.com>
2021-12-14 12:13:34 +0200

    Add project files.

---
+++ HomeWorkBasic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
